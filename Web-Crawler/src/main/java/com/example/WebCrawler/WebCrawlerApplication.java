a768a6145f32fb3821fd0f58f59ad58620eb10e0
Saliha <saliha0768@gmail.com>
2024-04-04 12:05:54 +0300

    First attempt

--- Web-Crawler/src/main/java/com/example/WebCrawler/WebCrawlerApplication.java
+++ Web-Crawler/src/main/java/com/example/WebCrawler/WebCrawlerApplication.java
@@ -1,13 +1,19 @@
 package com.example.WebCrawler;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@ComponentScan(basePackages = {"com.example.WebCrawler.Repositories"})
 public class WebCrawlerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebCrawlerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebCrawlerApplication.class, args);
+    }
 
 }
