0e5237f33fc2915769e46745393a089a94e252f5
Saliha <saliha0768@gmail.com>
2024-04-13 16:15:09 +0300

    new files

--- src/main/java/com/example/WebCrawler/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.WebCrawler.Repositories;
-import com.example.WebCrawler.Entities.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-public interface UserRepository extends CrudRepository<User,Long> {
-    User getUserByUsername(@Param("username") String username);
-
-}
